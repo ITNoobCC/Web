14cb4411b9e03bc9583d5ed9e5be91f43215d703
Vlad <flipflike@bk.ru>
2019-09-10 17:39:20 +0300

    Fix code js

--- Lab1/js/searchCard.js
+++ Lab1/js/searchCard.js
@@ -1,28 +1,11 @@
-//The following code is based off a toggle menu by @Bradcomp
-//source: https:gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function () {
-  var burger = document.querySelector('.burger');
-  var menu = document.querySelector('#' + burger.dataset.target);
-  burger.addEventListener('click', function () {
-    burger.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
-  });
-});
-
 document.querySelector('#card').oninput = function () {
   let val = this.value.trim();
   let cardItems = document.querySelectorAll('.card');
-  if (val != '') {
-    cardItems.forEach(function (element) {
-      if (element.innerText.search(val) == -1) {
-        element.classList.add('hide');
-      } else {
-        element.classList.remove('hide');
-      }
-    });
-  } else {
-    cardItems.forEach(function (element) {
+  cardItems.forEach(function (element) {
+    if ((val !== '') && (element.innerText.search(val) === -1)) {
+      element.classList.add('hide');
+    } else {
       element.classList.remove('hide');
-    });
-  }
+    }
+  });
 }
