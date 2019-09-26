64ab55363cd2d30e36b1e1b5cc005e44f4df4235
Vlad <flipflike@bk.ru>
2019-09-26 12:59:08 +0300

    Complete All

--- Lab1/js/searchCard.js
+++ Lab1/js/searchCard.js
@@ -3,9 +3,9 @@ document.querySelector("#column").oninput = function() {
   let columnItems = document.querySelectorAll(".column");
   columnItems.forEach(function(element) {
     if (val !== "" && element.innerText.search(val) === -1) {
-      element.classList.add("hide");
+      element.classList.add('hide');
     } else {
-      element.classList.remove("hide");
+      element.classList.remove('hide');
     }
   });
 };
--- Lab2/js/app.js
+++ Lab2/js/app.js
@@ -1,65 +1,124 @@
-window.onload = function () {
+window.onload = function() {
   var btn = document.getElementById('button');
   var txtArea = document.getElementById('textarea');
 
-  // add task handler
-  btn.onclick = function () {
-    // add element to UI
-    var taskContent = txtArea.value;
-    var containerToDo = document.getElementById('container-to-do');
-    var newTask = document.createElement('div');
-    newTask.classList.add('new', 'checkbox');
-    containerToDo.append(newTask);
-    var label = document.createElement('label');
-    var check = document.createElement('input');
-    check.setAttribute('type', 'checkbox');
-    label.append(check);
-    newTask.append(label);
-    var text = document.createTextNode(taskContent);
-    label.append(text);
-    var icon = document.createElement('i');
-    icon.classList.add('glyphicon', 'glyphicon-trash', 'icon', 'remove-card');
-    newTask.append(icon);
-    txtArea.value = "";
+  btn.onclick = function() {
+    if (txtArea.value != '') {
 
-    // register events here:
+      var taskContent = txtArea.value;
+      var containerToDo = document.getElementById('container-to-do');
+      var newTask = document.createElement('div');
+      newTask.classList.add('new', 'checkbox', 'view');
+      containerToDo.append(newTask);
+      var label = document.createElement('label');
+      var check = document.createElement('input');
+      check.setAttribute('type', 'checkbox');
+      check.classList.add('task-checkbox', 'toggle');
+      label.append(check);
+      newTask.append(label);
+      var text = document.createTextNode(taskContent);
+      label.append(text);
+      label.classList.add('task-label');
+      var icon = document.createElement('i');
+      icon.append('Delete');
+      icon.classList.add('glyphicon', 'glyphicon-trash', 'icon', 'remove-card');
+      newTask.append(icon);
+      txtArea.value = '';
 
-    // complete task
-    // TODO: labeled - not valid name for CSS and context
-    check.onclick = function () {
-      label.classList.toggle('labeled');
-    };
+      check.onclick = function() {
+        if (!label.parentElement.classList.contains('deleted')) {
+          label.classList.toggle('completed');
+          label.parentElement.classList.toggle('completed');
+        } else {
+          label.classList.remove('completed');
+          label.parentElement.classList.remove('completed');
+          label.children[0].checked = false;
+        }
+      };
 
-    // TODO: delete task event
-    icon.onclick = function () {
-      console.log('delete');
-    };
+      icon.onclick = function(element) {
+        if (!element.currentTarget.parentElement.classList.contains('deleted')) {
+          element.currentTarget.parentElement.style.display = 'none';
+        }
+
+        element.currentTarget.parentElement.classList.add('deleted');
+      };
+    }
   };
 
-  // TODO: complete all handler here
-  /*
-    var completeAll = document.getElementById('complete-all');
-    completeAll.onclick = function () {
-      console.log('complete all');
-    };
-  */
+  var completeAll = document.getElementById('complete-all');
+  completeAll.onclick = function() {
+    var massTaskList = document.querySelectorAll('.new');
 
+    for (let items of massTaskList) {
 
-  // TODO: filter tasks
-  /*
-    var filterShowAll = document.getElementById('filter-show-all');
-    filterShowAll.onclick = function () {
-      console.log('show all');
-    };
+      if (!items.classList.contains('deleted') && completeAll.checked) {
+        items.classList.add('completed');
+      } else {
+        items.classList.remove('completed');
+      }
 
-    var filterShowCompleted = document.getElementById('filter-show-completed');
-    filterShowCompleted.onclick = function () {
-      console.log('show completed');
-    };
+      let taskLabel = items.children[0];
+      let taskCheck = taskLabel.children[0];
 
-    var filterShowCompleted = document.getElementById('filter-show-removed');
-    filterShowRemoved.onclick = function () {
-      console.log('show removed');
-    };
-  */
+      if (items.classList.contains('completed')) {
+        taskLabel.classList.add('completed');
+        taskCheck.checked = true;
+      } else {
+        taskLabel.classList.remove('completed');
+        taskCheck.checked = false;
+      }
+
+      taskCheck.onclick = function(element) {
+        completeAll.checked = false;
+
+        if (element.target.parentElement.classList.contains('completed')) {
+          element.target.parentElement.classList.remove('completed');
+          element.target.parentElement.parentElement.classList.remove('completed');
+        } else {
+          element.target.parentElement.classList.add('completed');
+          element.target.parentElement.parentElement.classList.add('completed');
+        }
+      };
+    }
+  };
+
+  var filterShowAll = document.getElementById('filter-show-all');
+  filterShowAll.onclick = function() {
+    var massTaskList = document.querySelectorAll('.new');
+
+    for (let items of massTaskList) {
+      if (!items.classList.contains('deleted')) {
+        items.style.display = 'block';
+      } else {
+        items.style.display = 'none';
+      }
+    }
+  };
+
+  var filterShowCompleted = document.getElementById('filter-show-completed');
+  filterShowCompleted.onclick = function() {
+    var massTaskList = document.querySelectorAll('.new');
+
+    for (let items of massTaskList) {
+      if (!items.classList.contains('deleted') && items.classList.contains('completed')) {
+        items.style.display = 'block';
+      } else {
+        items.style.display = 'none';
+      }
+    }
+  };
+
+  var filterShowRemoved = document.getElementById('filter-show-removed');
+  filterShowRemoved.onclick = function() {
+    var massTaskList = document.querySelectorAll('.new');
+
+    for (let items of massTaskList) {
+      if (items.classList.contains('deleted')) {
+        items.style.display = 'block';
+      } else {
+        items.style.display = 'none';
+      }
+    }
+  };
 }
