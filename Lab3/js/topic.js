734bb995583bdb1d186fbccdd9e0a5f39312020f
Vlad <flipflike@bk.ru>
2019-11-05 17:15:40 +0300

    Fix black-white

--- Lab3/js/topic.js
+++ Lab3/js/topic.js
@@ -1,3 +1,12 @@
 window.onload = function() {
   var blackWhite = document.getElementById('black-white');
+  blackWhite.onclick = function() {
+    if (blackWhite.checked == true) {
+      var darkTopic = document.getElementById('calendar');
+      darkTopic.classList.remove('dark-topic');
+    } else {
+      var darkTopic = document.getElementById('calendar');
+      darkTopic.classList.toggle('dark-topic');
+    }
+  };
 };
