bd33e24f5a445174376cf3d2523b2f7167c59c28
Volodya <flipflike@bk.ru>
2019-11-12 17:14:03 +0300

    fix lw4

--- src/Lab5/Task4/Task4.js
+++ src/Lab5/Task4/Task4.js
@@ -1,8 +1,8 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
   if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
     if (
-      (timeRange1[0] > timeRange2[0] && timeRange1[0] < timeRange2[1]) ||
-      (timeRange2[0] > timeRange1[0] && timeRange2[0] < timeRange1[1])
+      (timeRange1[0] >= timeRange2[0] && timeRange1[0] <= timeRange2[1]) ||
+      (timeRange2[0] >= timeRange1[0] && timeRange2[0] <= timeRange1[1])
     ) {
       return true;
     } else {
@@ -11,5 +11,5 @@ function isTimeRangesIntersect(timeRange1, timeRange2) {
   }
 }
 
-console.log(isTimeRangesIntersect(['18:30', '19:30'], ['19:00', '21:00'])); // return true
-console.log(isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00'])); // return false
+console.log(isTimeRangesIntersect(["18:30", "19:30"], ["19:00", "21:00"])); // return true
+console.log(isTimeRangesIntersect(["08:30", "09:30"], ["10:30", "12:00"])); // return false
