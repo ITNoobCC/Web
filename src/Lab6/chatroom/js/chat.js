511603cb8138b1170952574be22577b6cb3333c9
Volodya <flipflike@bk.ru>
2019-11-19 16:24:03 +0300

    FixNpm

--- src/Lab6/chatroom/js/chat.js
+++ src/Lab6/chatroom/js/chat.js
@@ -9,17 +9,27 @@ window.onload = function() {
       var chatBlock = document.getElementById('chat-block');
       var newMessageBlock = document.createElement('div');
       var textMessage = document.createElement('p');
+      var userName = document.createElement('p');
+      
       if (userTag.checked === true) {
         newMessageBlock.classList.add('you-message');
         textMessage.classList.add('you-message_text');
         textMessage.append(messageContent);
+
+        userName.classList.add('you-message_userName');
+        userName.append('User - 1');
+       
       } else {
         newMessageBlock.classList.add('interlocutor-message');
         textMessage.classList.add('interlocutor-message_text');
         textMessage.append(messageContent);
+
+        userName.classList.add('interlocutor-message_userName');
+        userName.append('User - 2');
       }
       chatBlock.append(newMessageBlock);
       newMessageBlock.append(textMessage);
+      newMessageBlock.append(userName);
       txtArea.value = '';
     }
   }
